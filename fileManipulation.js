25effe5ebda6298e13d2488d9e5db8382dde7dae
Manoli <uyamis.rahou@gmail.com>
2023-05-25 13:46:59 +0200

    handled concurrency for temp files added some error handling

--- fileManipulation.js
+++ fileManipulation.js
@@ -8,26 +8,37 @@ export async function printAttachments(stream) {
   
     if(mail.attachments.length === 0) return;
 
-    for(let i = 0; i < mail?.attachments.length; i++){
-        const file = mail?.attachments.at(i);
-        const path = `${config.filePath}/unknown${i}`;
+    mail.attachments.forEach(file => {
+        const path = `${config.filePath}/temp${idMaker()}`;
         if (!fs.existsSync(config.filePath)) {
             fs.mkdirSync(config.filePath);
         }
         fs.writeFileSync(path, file.content, "binary");
         printFile(path);
-    }
+    })
 }
+
+function* idMaker() {
+    let index = 0;
+    while (true) {
+      yield index++;
+      if(index >= 60 * 60 * 20) index = 0;
+    }
+  }
 
 function printFile(path){
     printer.print(path).then(value => {
         console.log("sent")
         deleteFile(path);
-    }).catch(err => {
-        console.log(`error : ${err}`)
+    }).catch(error => {
+        console.log(`An error has occured when printing the file ${path} \n ${error}`)
     });
 }
 
 function deleteFile(path){
-    fs.unlinkSync(path)
+    try {
+        fs.unlinkSync(path)
+    } catch (error) {
+        console.log(`An error has occured when deleting the file ${path} \n ${err}`)
+    }
 }
